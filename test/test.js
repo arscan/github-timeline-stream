692a436147cbbc6e95f13081442141eea8e8177f
Rob Scanlon <robscanlon@gmail.com>
2014-04-19 22:05:51 -0400

    made initial version

--- test/test.js
+++ test/test.js
@@ -4,52 +4,49 @@ var GithubTimelineStream = require('../lib/GithubTimelineStream.js');
 var through = require("through");
 
 suite('Stream Test', function(){
-    var gth = new GithubTimelineStream();
-    test('We can get at least 50 out', function(done){
+    test('We can get at least 31 out', function(done){
+        var gth = new GithubTimelineStream();
         var count = 0;
         var finished = false;
 
         this.timeout(15000);
-        console.log(count);
 
 
-        gth.pipe(through(function(data){
-                console.log(data);
+        gth.pipe(through(function(){
                 count++;
-                if(count > 50 && !finished){
+                if(count > 30 && !finished){
                     finished = true;
+                    gth = null;
                     done();
                 }
 
             })
         );
 
-        /*
-        gth.on('data', function(){
-        });
-       */
     });
 
     test('We dont get any dups', function(done){
+        var gth = new GithubTimelineStream();
 
         var ids = {};
         var count= 0;
         var finished = false;
 
-        this.timeout(13000);
+        this.timeout(15000);
 
 
         gth.on('data', function(data){
-            console.log("GETTING STUFF");
             count++;
             if(ids[data.url]){
                 done("Found duplicate item!");
             }
+            console.log(data.url);
 
             ids[data.url] = true;
-            if(count > 50 && !finished){
+            if(count > 30 && !finished){
                 finished = true;
                 done();
+                gth = null;
             }
         });
     });
