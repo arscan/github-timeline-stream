a2746ee64e0c667dd54651a9bee03ad2bf9966b9
Rob Scanlon <robscanlon@gmail.com>
2014-04-19 21:06:05 -0400

    testing with a transform stream, but dont think i can do it because the source streams end.

--- test/test.js
+++ test/test.js
@@ -1,24 +1,56 @@
 'use strict';
 
 var GithubTimelineStream = require('../lib/GithubTimelineStream.js');
+var through = require("through");
 
 suite('Stream Test', function(){
+    var gth = new GithubTimelineStream();
     test('We can get at least 50 out', function(done){
-        var gth = new GithubTimelineStream();
         var count = 0;
         var finished = false;
 
-        this.timeout(10000);
+        this.timeout(15000);
+        console.log(count);
+
+
+        gth.pipe(through(function(data){
+                console.log(data);
+                count++;
+                if(count > 50 && !finished){
+                    finished = true;
+                    done();
+                }
+
+            })
+        );
+
+        /*
+        gth.on('data', function(){
+        });
+       */
+    });
+
+    test('We dont get any dups', function(done){
+
+        var ids = {};
+        var count= 0;
+        var finished = false;
+
+        this.timeout(13000);
 
 
         gth.on('data', function(data){
-            console.log(data);
+            console.log("GETTING STUFF");
             count++;
+            if(ids[data.url]){
+                done("Found duplicate item!");
+            }
+
+            ids[data.url] = true;
             if(count > 50 && !finished){
                 finished = true;
                 done();
             }
         });
-    
     });
 });
